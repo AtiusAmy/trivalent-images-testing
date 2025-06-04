c5d46a33fa5bbf648e9ebb877e6d4e124815ad91
Atius Amy <torv.device027@passmail.net>
2025-06-05 05:01:31 +0700

    no SELinux for now

--- build_files/build.sh
+++ build_files/build.sh
@@ -24,22 +24,22 @@ dnf -y --enablerepo "secureblue" --nogpgcheck  install trivalent
 
 #### Example for enabling a System Unit File
 
-echo "Installing secureblue Trivalent selinux policy"
-echo "Install 'selinux-policy-devel' build package & it's dependencies"
-dnf -y install selinux-policy-devel
-echo "Downloading secureblue Trivalent selinux policy"
-mkdir -p ./selinux/trivalent
-cd ./selinux/trivalent
-SELINUX_POLICY_URL="https://raw.githubusercontent.com/secureblue/secureblue/refs/heads/live/files/scripts/selinux/trivalent"
-curl -fLs --create-dirs -O "${SELINUX_POLICY_URL}/trivalent.fc" --output-dir "${PWD}"
-curl -fLs --create-dirs -O "${SELINUX_POLICY_URL}/trivalent.if" --output-dir "${PWD}"
-curl -fLs --create-dirs -O "${SELINUX_POLICY_URL}/trivalent.te" --output-dir "${PWD}"
-curl -fLs --create-dirs -O "${SELINUX_POLICY_URL}/trivalent.sh" --output-dir "${PWD}"
-echo "Executing trivalent.sh script"
-bash "${PWD}/trivalent.sh"
-cd ../..
-echo "Cleaning up build package 'selinux-policy-devel' & it's dependencies"
-dnf -y remove selinux-policy-devel
+#echo "Installing secureblue Trivalent selinux policy"
+#echo "Install 'selinux-policy-devel' build package & it's dependencies"
+#dnf -y install selinux-policy-devel
+#echo "Downloading secureblue Trivalent selinux policy"
+#mkdir -p ./selinux/trivalent
+#cd ./selinux/trivalent
+#SELINUX_POLICY_URL="https://raw.githubusercontent.com/secureblue/secureblue/refs/heads/live/files/scripts/selinux/trivalent"
+#curl -fLs --create-dirs -O "${SELINUX_POLICY_URL}/trivalent.fc" --output-dir "${PWD}"
+#curl -fLs --create-dirs -O "${SELINUX_POLICY_URL}/trivalent.if" --output-dir "${PWD}"
+#curl -fLs --create-dirs -O "${SELINUX_POLICY_URL}/trivalent.te" --output-dir "${PWD}"
+#curl -fLs --create-dirs -O "${SELINUX_POLICY_URL}/trivalent.sh" --output-dir "${PWD}"
+#echo "Executing trivalent.sh script"
+#bash "${PWD}/trivalent.sh"
+#cd ../..
+#echo "Cleaning up build package 'selinux-policy-devel' & it's dependencies"
+#dnf -y remove selinux-policy-devel
 
 echo "Assure that network sandbox is always disabled by default (to ensure that login data remains)"
 echo "https://github.com/fedora-silverblue/issue-tracker/issues/603"
